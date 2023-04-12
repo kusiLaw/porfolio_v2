d475a3b5e6c6ccf865b0887da65957c87f26ec68
kusi <lawcubegsm@gmail.com>
2023-04-12 19:40:22 +0000

    Add page layout

---
+++ components/layout/layout.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+
+export default function Layout({ children }) {
+ return (
+   <div className='max-w-screen-lg '>
+     <Head>
+       <meta name="viewport" content="width=device-width, initial-scale=1" />
+       <link rel="icon" href="/favicon.ico" />
+       <title>Lawrence Kusi Portfolio</title>
+       <meta name="description"  content="Lawrence Addai Kusi Portfolio | Modern portfolio website"/>
+     </Head>
+     <main>{children}</main>
+
+   </div>
+ );
+}
