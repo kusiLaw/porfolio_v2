446c4cac55b63725006ea17aeee7d66e9f25a559
kusi <lawcubegsm@gmail.com>
2023-04-13 06:39:00 +0000

    Add nav component

---
+++ components/header/nav.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+const Nav = ({desktop}) =>{
+ return (
+     <nav className="flex sm:justify-center space-x-2">
+      {[
+        ['Home', '/'],
+        ['About', '#about'],
+        ['Project', '#projects'],
+        ['Contact', '#contact'],
+      ].map(([title, url], index) => (
+        <Link key={index} href={url}  className="
+             rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">
+                <span className='
+                inline-block mr-3  text-light_accent_dark italic'> 0{index + 1}.</span> 
+                <span className='dark:text-dark_text_header text-xl '>{title}</span>
+        </Link>
+      ))}
+     </nav>
+ )
+}
+
+
+export default Nav
