7a044adb362f1b1c910ca38eac906e9dc226772b
kusi <lawcubegsm@gmail.com>
2023-04-16 05:30:06 +0000

    Add title component

---
+++ components/title/title.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Title = ({text}) => {
+  return (
+    <div className='flex w-full text-2xl font-bold md:text-4xl 
+         text-light_text_header dark:text-dark_text_header'>
+         <h2 className="inline-block after:inline-block after:content-[''] after:bg-light_accent after:h-1">
+         {text}
+
+         {/* flex items-center gap-2 */}
+         </h2>
+    </div>
+  )
+}
+
+
+const SubTile = ({text}) => {
+ return (
+   <div className=''>
+        <h3 className='text-xl 
+        font-bold md:text-4xl text-light_text_header dark:text-dark_text_header'>
+        {text}
+        </h3>
+   </div>
+ )
+}
+
+export {Title, SubTile}
